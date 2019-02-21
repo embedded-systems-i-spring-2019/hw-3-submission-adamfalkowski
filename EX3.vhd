d725ae333c33f9370fe12307996a7a040bcf6cd5
adamfalkowski <47360465+adamfalkowski@users.noreply.github.com>
2019-02-20 19:10:37 -0500

    Update EX3.vhd

--- EX3.vhd
+++ EX3.vhd
@@ -6,7 +6,7 @@ use IEEE.std_logic_1164.ALL;
 entity mux_2t1 is
   port(
       X,Y : in std_logic_vector(7 downto 0);
-      mux_out : out std_logic_vector(7 downto 0)
+      mux_out : out std_logic_vector(7 downto 0);
       SEL : in std_logic;
        );
 end mux_2t1;
@@ -46,7 +46,7 @@ architecture reg_arc of reg is
        end if;
     end process;
 end reg_arc;          
- 
+--entity declaration for crk_ex3----------------------------------------------------- 
 entity crk_ex3 is
   port(
         LDB,LDA,S0,S1,CLK : in std_logic;
@@ -54,6 +54,58 @@ entity crk_ex3 is
         RB : out std_logic_vector (7 downto 0)
        );
 end crk_ex3;
+  
+architecture crk_ex3_arc of crk_ex3 is
+  --component declaration
+  component mux2t1
+    port( 
+          A,B : in std_logic_vector(7 downto 0);
+          SEL : in std_logic;
+          mux_out: out std_logic_vector(7 downto 0)
+         );
+    end component;
+  
+   component reg
+    port( 
+        R_in : in std_logic_vector(7 downto 0);
+        CLK,LD : in std_logic;
+        R_out: out std_logic_vector (7 downto 0)); 
+  end component;
+  --intermediate signal declarations
+  signal mux_result_1, mux_result_2, REGA_result, REGB_result : std_logic_vector(7 downto 0);
+  begin
+    Mux1_2_to_1: mux2t1
+                        port map (
+                                    A => X,
+                                    B => REGB_result,
+                                    SEL => S0,
+                                    mux_out => mux_result_1
+                                  );
+     Mux2_2_to_1: mux2t1
+                        port map (
+                                    A => REGA_result,
+                                    B => Y,
+                                    SEL => S1,
+                                    mux_out => mux_result_2
+                                  );
+     REGA: reg
+                        port map (
+                                    R_in => mux_result_1,
+                                    CLK => CLK,
+                                    LD => LDA,
+                                    R_out => REGA_result
+                                  );
+      REGB: reg
+                        port map (
+                                    R_in => mux_result_2,
+                                    CLK => CLK,
+                                    LD => LDB,
+                                    R_out => REGB_result
+                                  );   
+      RB <= REGB_result;
+ end crk_ex3; 
+         
+  
   
     
 
